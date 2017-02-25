0453b0563c305bafe1adb4318a34e41a3bbd99f3
Nicolas Mora <mail@babelouest.org>
2017-02-25 13:29:53 -0500

    Larger icons, add a force refresh button for after sleep mode

--- src/components/topRightMenu/topRightMenu.component.js
+++ src/components/topRightMenu/topRightMenu.component.js
@@ -1,58 +1,64 @@
-function topRightMenuCtrl ($scope, $rootScope, $location, $http, $translate, $cookies, angharadFactory, sagremorConstant, sagremorParams, angharadConfig) {
-    var self = this;
+function topRightMenuCtrl($scope, $rootScope, $location, $http, $translate, $cookies, angharadFactory, sagremorConstant, sagremorParams, angharadConfig) {
+	var self = this;
 
-    self.sagremorParams = sagremorParams;
+	self.sagremorParams = sagremorParams;
 
-    self.selectedLang = $translate.use();
-    
-    self.langList = sagremorConstant.langList;
-    
-    self.profiles = sagremorParams.profiles;
-    self.currentProfileName = !!sagremorParams.currentProfile?sagremorParams.currentProfile.name:"";
+	self.selectedLang = $translate.use();
 
-    self.oauth2 = angharadConfig.oauth2;
-    
-    if ($location.search().type) {
-        self.oauth2.responseType = $location.search().type;
-    }
+	self.langList = sagremorConstant.langList;
 
-    self.changeLang = function () {
-        $translate.use(self.selectedLang).then(function () {
+	self.profiles = sagremorParams.profiles;
+	self.currentProfileName = !!sagremorParams.currentProfile ? sagremorParams.currentProfile.name : "";
+
+	self.oauth2 = angharadConfig.oauth2;
+
+	if ($location.search().type) {
+		self.oauth2.responseType = $location.search().type;
+	}
+
+	self.changeLang = function() {
+		$translate.use(self.selectedLang).then(function() {
 			$rootScope.$broadcast("changeLang");
 		});
-    };
-    
-    self.changeProfile = function () {
-		_.forEach(sagremorParams.profiles, function (profile) {
+	};
+
+	self.changeProfile = function() {
+		_.forEach(sagremorParams.profiles, function(profile) {
 			if (profile.name === self.currentProfileName) {
 				sagremorParams.currentProfile = profile;
 				var now = new Date();
-				var exp = new Date(now.getFullYear()+10, now.getMonth(), now.getDate());
-				$cookies.put("ANGHARAD_PROFILE", self.currentProfileName, {expires: exp});
+				var exp = new Date(now.getFullYear() + 10, now.getMonth(), now.getDate());
+				$cookies.put("ANGHARAD_PROFILE", self.currentProfileName, {
+					expires: exp
+				});
 				$rootScope.$broadcast("angharadProfileUpdated");
 				$rootScope.$broadcast("refreshDashboard");
 			}
 		});
 	};
-	
-	self.refresh = function () {
+
+	self.refresh = function() {
 		$rootScope.$broadcast("refresh");
 	};
-	
-	$scope.$on("angharadProfileChanged", function () {
+
+	self.reconnect = function() {
+		$rootScope.$broadcast("reconnect");
+	};
+
+	$scope.$on("angharadProfileChanged", function() {
 		self.profiles = sagremorParams.profiles;
-		self.currentProfileName = !!sagremorParams.currentProfile?sagremorParams.currentProfile.name:"";
+		self.currentProfileName = !!sagremorParams.currentProfile ? sagremorParams.currentProfile.name : "";
 	});
-	
-	$scope.$on("angharadProfileUpdated", function () {
+
+	$scope.$on("angharadProfileUpdated", function() {
 		self.profiles = sagremorParams.profiles;
-		self.currentProfileName = !!sagremorParams.currentProfile?sagremorParams.currentProfile.name:"";
+		self.currentProfileName = !!sagremorParams.currentProfile ? sagremorParams.currentProfile.name : "";
 	});
 
 }
 
 angular.module("sagremorApp").component("topRightMenu", {
-    templateUrl: "components/topRightMenu/topRightMenu.template.html",
-    controller: topRightMenuCtrl,
-    controllerAs: "topRightMenuCtrl"
+	templateUrl: "components/topRightMenu/topRightMenu.template.html",
+	controller: topRightMenuCtrl,
+	controllerAs: "topRightMenuCtrl"
 });
